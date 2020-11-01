46194e391308acf5fa538b8a5462a2543d5fc455
Muhd Saifulnizam <muhdsaifulnizam1998@gmail.com>
2020-11-01 23:37:32 +0800

    feat: 🎸 Added SASS styling file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/assets/js/')
+    .sass('resources/assets/sass/crater.scss', 'public/assets/css/')
+
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: 'source-map'
+    }).sourceMaps()
+} else {
+    mix.version()
+}
