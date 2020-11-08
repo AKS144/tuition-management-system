9e4014cb6ae66096924c578d517caf5261561fa6
Muhd Saifulnizam <muhdsaifulnizam1998@gmail.com>
2020-11-09 01:55:11 +0800

    feat: 🎸 Enable axios response interceptor

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -60,57 +60,57 @@ window.axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest'
 }
 
-// /**
-//  * Interceptors
-//  */
+/**
+ * Interceptors
+ */
 
-// window.axios.interceptors.request.use(function (config) {
-//     // Do something before request is sent
-//     const AUTH_TOKEN = Ls.get('auth.token')
-//     // const companyId = Ls.get('selectedCompany')
+window.axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    const AUTH_TOKEN = Ls.get('auth.token')
+    // const companyId = Ls.get('selectedCompany')
 
-//     if (AUTH_TOKEN) {
-//         config.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
-//     }
+    if (AUTH_TOKEN) {
+        config.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
+    }
 
-//     // if (companyId) {
-//     //   config.headers.common['company'] = companyId
-//     // }
+    // if (companyId) {
+    //   config.headers.common['company'] = companyId
+    // }
 
-//     return config
-//     }, function (error) {
-//     // Do something with request error
-//     return Promise.reject(error)
-// })
+    return config
+    }, function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+})
 
-// /**
-//  * Global Axios Response Interceptor
-//  */
+/**
+ * Global Axios Response Interceptor
+ */
 
-// global.axios.interceptors.response.use(undefined, function (err) {
-//     // Do something with request error
-//     if (!err.response) {
-//         window.toastr['error']('Network error: Please check your internet connection or wait until servers are back online')
-//         console.log('Network error: Please check your internet connection.')
-//     } else {
-//         console.log(err.response)
-//         if (err.response.data && (err.response.statusText === 'Unauthorized' || err.response.data === ' Unauthorized.')) {
-//         // Unauthorized and log out
-//         window.toastr['error']((err.response.data.message) ? err.response.data.message : 'Unauthorized')
-//         // store.dispatch('auth/logout', true)
-//         } else if (err.response.data.errors) {
-//         // Show a notification per error
-//         const errors = JSON.parse(JSON.stringify(err.response.data.errors))
-//         for (const i in errors) {
-//             window.toastr['error'](errors[i])
-//         }
-//         } else {
-//         // Unknown error
-//         window.toastr['error']((err.response.data.message) ? err.response.data.message : 'Unknown error occurred')
-//         }
-//     }
-//     return Promise.reject(err)
-// })
+global.axios.interceptors.response.use(undefined, function (err) {
+    // Do something with request error
+    if (!err.response) {
+        window.toastr['error']('Network error: Please check your internet connection or wait until servers are back online')
+        console.log('Network error: Please check your internet connection.')
+    } else {
+        console.log(err.response)
+        if (err.response.data && (err.response.statusText === 'Unauthorized' || err.response.data === ' Unauthorized.')) {
+        // Unauthorized and log out
+        window.toastr['error']((err.response.data.message) ? err.response.data.message : 'Unauthorized')
+        // store.dispatch('auth/logout', true)
+        } else if (err.response.data.errors) {
+        // Show a notification per error
+        const errors = JSON.parse(JSON.stringify(err.response.data.errors))
+        for (const i in errors) {
+            window.toastr['error'](errors[i])
+        }
+        } else {
+        // Unknown error
+        window.toastr['error']((err.response.data.message) ? err.response.data.message : 'Unknown error occurred')
+        }
+    }
+    return Promise.reject(err)
+})
 
 /**
  * Global plugins
