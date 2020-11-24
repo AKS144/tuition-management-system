398beff7b8b28faa9fef5e471d979b752c394de4
Malik Ridhwan <mlikr@icloud.com>
2020-11-24 13:52:57 +0800

    Added ExpenseCategoriesSeeder

---
+++ database/seeds/ExpenseCategoriesSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\ExpenseCategory;
+
+class ExpenseCategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $expenseCategories = [
+            [
+                'name' => 'Utilities'
+            ],
+            [
+                'name' => 'Food and Beverages'
+            ],
+            [
+                'name' => 'Stationeries'
+            ],
+            [
+                'name' => 'Rental'
+            ]
+        ];
+
+        foreach ($expenseCategories as $expensecategory) {
+            ExpenseCategory::create($expensecategory);
+        }
+    }
+}
